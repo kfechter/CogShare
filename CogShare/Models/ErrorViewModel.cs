3412fddea1848eba00038f9b7bf5a8cb57d44809
Kenneth Fechter <kenneth.fechter@gmail.com>
2020-11-08 13:11:48 -0500

    Add Project

---
+++ CogShare/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CogShare.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
