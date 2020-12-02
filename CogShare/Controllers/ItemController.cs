257a35de3ca914b5e088be568df7378cae5fdbdb
Kenneth Fechter <kenneth.fechter@gmail.com>
2020-12-01 22:53:18 -0500

    Implement Item View Model

--- CogShare/Controllers/ItemController.cs
+++ CogShare/Controllers/ItemController.cs
@@ -1,20 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CogShare.Domain.Entities;
+using CogShare.EFCore;
+using CogShare.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace CogShare.Controllers
 {
     public class ItemController : Controller
     {
         private readonly ILogger<ItemController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly CogShareContext _cogShareContext;
 
-        public ItemController(ILogger<ItemController> logger)
+        public ItemController(ILogger<ItemController> logger, UserManager<ApplicationUser> userManager, CogShareContext cogShareContext)
         {
             _logger = logger;
+            _userManager = userManager;
+            _cogShareContext = cogShareContext;
         }
 
         public IActionResult Items()
         {
-            return View();
+            var user = _userManager.GetUserAsync(HttpContext.User).Result; // get the user ID and do the rest with db context.
+            var items = _cogShareContext.Items.Include(x => x.Borrower).Where(x => x.Owner == user).ToList();
+            var itemViewModel = new ItemViewModel(items);
+            return View(itemViewModel);
         }
 
         public IActionResult Requests()
--- CogShare/Models/ItemViewModel.cs
+++ CogShare/Models/ItemViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CogShare.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,31 @@ namespace CogShare.Models
 {
     public class ItemViewModel
     {
+        public bool ErrorState { get; set; }
+
+        public string StatusMessage { get; set; }
+
+        public List<Item> Items { get; set; }
+
+        public ItemViewModel()
+        {
+            ErrorState = false;
+            StatusMessage = string.Empty;
+            Items = new List<Item>();
+        }
+
+        public ItemViewModel(List<Item> items)
+        {
+            Items = items;
+            ErrorState = false;
+            StatusMessage = string.Empty;
+        }
+
+        public ItemViewModel(bool errorState, string statusMessage, List<Item> items)
+        {
+            ErrorState = errorState;
+            StatusMessage = statusMessage;
+            Items = items;
+        }
     }
 }
